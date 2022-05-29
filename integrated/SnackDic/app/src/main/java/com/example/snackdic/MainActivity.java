80c99c50701d53511fef2f4b2647d23ff4033be8
retu-lab <rs30810@naver.com>
2022-05-30 01:14:00 +0900

    22.05.30_1
    mbti 기능 마무리
    -사진, 글 마무리

--- integrated/SnackDic/app/src/main/java/com/example/snackdic/MainActivity.java
+++ integrated/SnackDic/app/src/main/java/com/example/snackdic/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.example.snackdic.FBTI.testActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
